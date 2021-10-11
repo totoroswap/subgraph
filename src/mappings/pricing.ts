6fd180ea57073ff0ba778561ba16cc91fd936092
totoroswap <totoroswap@aol.com>
2021-10-11 15:39:07 +0800

    init

---
+++ src/mappings/pricing.ts
@@ -0,0 +1,156 @@
+/* eslint-disable prefer-const */
+import { Pair, Token, Bundle } from '../types/schema'
+import { BigDecimal, Address, BigInt } from '@graphprotocol/graph-ts/index'
+import { ZERO_BD, factoryContract, ADDRESS_ZERO, ONE_BD, UNTRACKED_PAIRS } from './helpers'
+
+const WETH_ADDRESS = '0x8f8526dbfd6e38e3d8307702ca8469bae6c56c15'
+const USDT_WETH_PAIR = '0x44c147d0c9623e49eabe8ae6ccd97d351e708dee' // created block 10093341
+
+export function getEthPriceInUSD(): BigDecimal {
+  let usdtPair = Pair.load(USDT_WETH_PAIR)
+  return usdtPair.token0Price
+}
+
+// token where amounts should contribute to tracked volume and liquidity
+let WHITELIST: string[] = [
+  '0x8f8526dbfd6e38e3d8307702ca8469bae6c56c15', // WOKT
+  '0x21cde7e32a6caf4742d00d44b07279e7596d26b9', // DAI
+  '0xc946daf81b08146b1c7a8da2a851ddf2b3eaaf85', // USDC
+  '0x382bb369d343125bfb2117af9c149795c6c65c50', // USDT
+  '0x6299b03a3B121E450BB173dE69186d54981e6cef', // IOS
+  '0xef71ca2ee68f45b9ad6f72fbdb33d707b872315c', // WETH
+  '0x54e4622dc504176b3bb432dccaf504569699a7ff', // WBTC
+  '0xfa520efc34c81bfc1e3dd48b7fe9ff326049f986' // WLTC
+]
+
+// minimum liquidity required to count towards tracked volume for pairs with small # of Lps
+let MINIMUM_USD_THRESHOLD_NEW_PAIRS = BigDecimal.fromString('400000')
+
+// minimum liquidity for price to get tracked
+let MINIMUM_LIQUIDITY_THRESHOLD_ETH = BigDecimal.fromString('2')
+
+/**
+ * Search through graph to find derived Eth per token.
+ * @todo update to be derived ETH (add stablecoin estimates)
+ **/
+export function findEthPerToken(token: Token): BigDecimal {
+  if (token.id == WETH_ADDRESS) {
+    return ONE_BD
+  }
+  // loop through whitelist and check if paired with any
+  for (let i = 0; i < WHITELIST.length; ++i) {
+    let pairAddress = factoryContract.getPair(Address.fromString(token.id), Address.fromString(WHITELIST[i]))
+    if (pairAddress.toHexString() != ADDRESS_ZERO) {
+      let pair = Pair.load(pairAddress.toHexString())
+      if (pair.token0 == token.id && pair.reserveETH.gt(MINIMUM_LIQUIDITY_THRESHOLD_ETH)) {
+        let token1 = Token.load(pair.token1)
+        return pair.token1Price.times(token1.derivedETH as BigDecimal) // return token1 per our token * Eth per token 1
+      }
+      if (pair.token1 == token.id && pair.reserveETH.gt(MINIMUM_LIQUIDITY_THRESHOLD_ETH)) {
+        let token0 = Token.load(pair.token0)
+        return pair.token0Price.times(token0.derivedETH as BigDecimal) // return token0 per our token * ETH per token 0
+      }
+    }
+  }
+  return ZERO_BD // nothing was found return 0
+}
+
+/**
+ * Accepts tokens and amounts, return tracked amount based on token whitelist
+ * If one token on whitelist, return amount in that token converted to USD.
+ * If both are, return average of two amounts
+ * If neither is, return 0
+ */
+export function getTrackedVolumeUSD(
+  tokenAmount0: BigDecimal,
+  token0: Token,
+  tokenAmount1: BigDecimal,
+  token1: Token,
+  pair: Pair
+): BigDecimal {
+  let bundle = Bundle.load('1')
+  let price0 = token0.derivedETH.times(bundle.ethPrice)
+  let price1 = token1.derivedETH.times(bundle.ethPrice)
+
+  // dont count tracked volume on these pairs - usually rebass tokens
+  if (UNTRACKED_PAIRS.includes(pair.id)) {
+    return ZERO_BD
+  }
+
+  // if less than 5 LPs, require high minimum reserve amount amount or return 0
+  if (pair.liquidityProviderCount.lt(BigInt.fromI32(5))) {
+    let reserve0USD = pair.reserve0.times(price0)
+    let reserve1USD = pair.reserve1.times(price1)
+    if (WHITELIST.includes(token0.id) && WHITELIST.includes(token1.id)) {
+      if (reserve0USD.plus(reserve1USD).lt(MINIMUM_USD_THRESHOLD_NEW_PAIRS)) {
+        return ZERO_BD
+      }
+    }
+    if (WHITELIST.includes(token0.id) && !WHITELIST.includes(token1.id)) {
+      if (reserve0USD.times(BigDecimal.fromString('2')).lt(MINIMUM_USD_THRESHOLD_NEW_PAIRS)) {
+        return ZERO_BD
+      }
+    }
+    if (!WHITELIST.includes(token0.id) && WHITELIST.includes(token1.id)) {
+      if (reserve1USD.times(BigDecimal.fromString('2')).lt(MINIMUM_USD_THRESHOLD_NEW_PAIRS)) {
+        return ZERO_BD
+      }
+    }
+  }
+
+  // both are whitelist tokens, take average of both amounts
+  if (WHITELIST.includes(token0.id) && WHITELIST.includes(token1.id)) {
+    return tokenAmount0
+      .times(price0)
+      .plus(tokenAmount1.times(price1))
+      .div(BigDecimal.fromString('2'))
+  }
+
+  // take full value of the whitelisted token amount
+  if (WHITELIST.includes(token0.id) && !WHITELIST.includes(token1.id)) {
+    return tokenAmount0.times(price0)
+  }
+
+  // take full value of the whitelisted token amount
+  if (!WHITELIST.includes(token0.id) && WHITELIST.includes(token1.id)) {
+    return tokenAmount1.times(price1)
+  }
+
+  // neither token is on white list, tracked volume is 0
+  return ZERO_BD
+}
+
+/**
+ * Accepts tokens and amounts, return tracked amount based on token whitelist
+ * If one token on whitelist, return amount in that token converted to USD * 2.
+ * If both are, return sum of two amounts
+ * If neither is, return 0
+ */
+export function getTrackedLiquidityUSD(
+  tokenAmount0: BigDecimal,
+  token0: Token,
+  tokenAmount1: BigDecimal,
+  token1: Token
+): BigDecimal {
+  let bundle = Bundle.load('1')
+  let price0 = token0.derivedETH.times(bundle.ethPrice)
+  let price1 = token1.derivedETH.times(bundle.ethPrice)
+
+  // both are whitelist tokens, take average of both amounts
+  if (WHITELIST.includes(token0.id) && WHITELIST.includes(token1.id)) {
+    return tokenAmount0.times(price0).plus(tokenAmount1.times(price1))
+  }
+
+  // take double value of the whitelisted token amount
+  if (WHITELIST.includes(token0.id) && !WHITELIST.includes(token1.id)) {
+    return tokenAmount0.times(price0).times(BigDecimal.fromString('2'))
+  }
+
+  // take double value of the whitelisted token amount
+  if (!WHITELIST.includes(token0.id) && WHITELIST.includes(token1.id)) {
+    return tokenAmount1.times(price1).times(BigDecimal.fromString('2'))
+  }
+
+  // neither token is on white list, tracked volume is 0
+  return ZERO_BD
+}
